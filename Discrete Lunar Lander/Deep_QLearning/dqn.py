3d13ee1b7d486b2184459b7992e54a8c2e687529
DavidAlexanderMoe <Davidalexander100@hotmail.com>
2024-06-17 12:14:02 +0200

    env

--- Discrete Lunar Lander/Deep_QLearning/dqn.py
+++ Discrete Lunar Lander/Deep_QLearning/dqn.py
@@ -1,3 +1,15 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import random
+import time
+import os
+import matplotlib.pyplot as plt
+from collections import deque
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 class QNet:
     pass
 
